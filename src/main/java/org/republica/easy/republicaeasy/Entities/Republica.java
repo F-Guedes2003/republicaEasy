65f7477fdfc3414c0238af248760ec6d3db3eb1e
Aislan <aislan.pepi@outlook.com>
2025-11-15 16:15:38 -0300

    feat: criado a entidade republica

---
+++ src/main/java/org/republica/easy/republicaeasy/Entities/Republica.java
@@ -0,0 +1,29 @@
+package org.republica.easy.republicaeasy.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class Republica {
+
+    @Id
+    private UUID id;
+    private String name;
+    private String description;
+    private String localization;
+    private String imageUrl;
+    private int limit;
+    private int contact;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
