a861218f4ef404a5a80d1162c87f5e150e67991f
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2025-11-15 17:34:25 -0300

    fix: rota de registro e geração do id automática no banco

--- src/main/java/org/republica/easy/republicaeasy/Entities/User.java
+++ src/main/java/org/republica/easy/republicaeasy/Entities/User.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 @Entity
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
     private String password;
